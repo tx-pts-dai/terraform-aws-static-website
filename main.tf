2f86c76da487a85a1e51e97f143736cd9b4a3665
Demetrio Carrara <demetrio.carrara@tx.group>
2023-12-27 23:02:50 +0100

    fix: allow cloudfront

--- main.tf
+++ main.tf
@@ -67,30 +67,30 @@ resource "aws_s3_object" "this" {
   etag         = each.value.digests.sha256
 }
 
+# Allow CloudFront to serve content from S3
+data "aws_iam_policy_document" "this" {
+  statement {
+    sid       = "AllowCloudFrontServicePrincipal"
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.this.arn}/*"]
 
-# data "aws_iam_policy_document" "this" {
-#   statement {
-#     sid       = "AllowCloudFrontServicePrincipal"
-#     actions   = ["s3:GetObject"]
-#     resources = ["${aws_s3_bucket.this.arn}/*"]
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.this.arn]
+    }
 
-#     condition {
-#       test     = "StringEquals"
-#       variable = "AWS:SourceArn"
-#       values   = [aws_cloudfront_distribution.this.arn]
-#     }
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+  }
+}
 
-#     principals {
-#       type        = "Service"
-#       identifiers = ["cloudfront.amazonaws.com"]
-#     }
-#   }
-# }
-
-# resource "aws_s3_bucket_policy" "this" {
-#   bucket = aws_s3_bucket.this.id
-#   policy = data.aws_iam_policy_document.this.json
-# }
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.id
+  policy = data.aws_iam_policy_document.this.json
+}
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
   bucket = aws_s3_bucket.this.bucket
