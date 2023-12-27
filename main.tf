32724989b76cc788936e64336aa31ef68ca41cae
Demetrio Carrara <demetrio.carrara@tx.group>
2023-12-27 22:59:11 +0100

    fix: allow for bucket private ACL

--- main.tf
+++ main.tf
@@ -23,9 +23,18 @@ resource "aws_s3_bucket" "this" {
   bucket = local.bucket_name
 }
 
+resource "aws_s3_bucket_ownership_controls" "this" {
+  bucket = aws_s3_bucket.this.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
 resource "aws_s3_bucket_acl" "this" {
   bucket = aws_s3_bucket.this.id
   acl    = "private"
+
+  depends_on = [aws_s3_bucket_ownership_controls.this]
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "candidates" {
